6a6f84c5461ad2b4cd6d2bc9a56fdfb457d0393b
Avina Paul <paul2aj@mail.uc.edu>
2026-06-15 15:28:10 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    int intOperandA = 8;
+    int intOperandB = 53;
+    int intSum = 61;
+    int intProduct = 424;
+    int intDifference = -45;
+    int intQuotient = 0;
+    int intModulo = 8;
+
+    double doubleOperandA = 4.75;
+    double doubleOperandB = 2.35;
+    double doubleSum = 7.1;
+    double doubleProduct = 11.16;
+    double doubleDifference = 2.4;
+    double doubleQuotient = 2.02;
+
+    // INT OPERATIONS AND OUTPUTS//
+    intSum = intOperandA + intOperandB;
+    System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+    intDifference = intOperandA - intOperandB;
+    System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+    intProduct = intOperandA * intOperandB;
+    System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+    intQuotient = intOperandA / intOperandB;
+    System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+    intModulo = intOperandA % intOperandB;
+    System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+    // DOUBLE OPERATIONS AND OUTPUTS
+    doubleSum = doubleOperandA + doubleOperandB;
+    System.out.println("The sum using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+    doubleDifference = doubleOperandA - doubleOperandB;
+    System.out.println("The difference using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+    doubleProduct = doubleOperandA * doubleOperandB;
+    System.out.println("The product using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+    doubleQuotient = doubleOperandA / doubleOperandB;
+    System.out.println("The quotient using ints of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+    //CREATING MY OWN VARIABLES
+    int numberOfKids = 0;
+    boolean rainingToday = false;
+    double gallonOfGasPrice = 4.15;
+    int favNumber = 4;
+    double shoeSize = 7.5;
+    String birthMonth = "November";
+    String fullName = "Avina Paul";
+
+
+
+
+    IO.println(String.format("Hello and welcome!"));
+
+
+
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = " + i);
+    }
+}
