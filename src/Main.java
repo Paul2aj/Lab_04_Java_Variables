37be54c3259ac1fa2ce19043f59ca87565050e1e
Avina Paul <paul2aj@mail.uc.edu>
2026-06-15 15:33:34 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -53,17 +53,4 @@ void main() {
     double shoeSize = 7.5;
     String birthMonth = "November";
     String fullName = "Avina Paul";
-
-
-
-
-    IO.println(String.format("Hello and welcome!"));
-
-
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
 }
